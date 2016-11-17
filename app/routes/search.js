8e9cefccdde35cff9a0cb6a72f2698392cca0dcc
John Branigan <jb@ulysses.local>
2016-11-16 19:34:10 -0500

    Reorganizes templates, updates styles

--- app/routes/search.js
+++ app/routes/search.js
@@ -6,7 +6,12 @@ var request = require('request');
 router.get('/:city', function(req, res, next) {
 	request('http://autocomplete.wunderground.com/aq?query=' + req.params.city, function (error, response, results) {
 	  if (!error && response.statusCode == 200) {
-		var cities = JSON.parse(results);
+		var data = JSON.parse(results);
+    var cities = {};
+    for (var i = 0; i < Object.keys(data.RESULTS).length; i++) {
+      cities[data.RESULTS[i].name] = data.RESULTS[i].l;
+      //console.log(data.RESULTS[i].name + " - " + data.RESULTS[i].l);
+    }		
 		res.send(cities);
 	  }
 	});
